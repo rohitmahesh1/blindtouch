2ed17e610094f15ec4fc593c50112ec263ce157f
Rohit Mahesh <imrohitmahesh1@gmail.com>
2026-06-18 20:51:00 -0500

    Add blindtouch ROS setup script

---
+++ ros2_ws/src/blindtouch_ros/setup.py
@@ -0,0 +1,25 @@
+from glob import glob
+
+from setuptools import find_packages, setup
+
+
+package_name = "blindtouch_ros"
+
+setup(
+    name=package_name,
+    version="0.1.0",
+    packages=find_packages(exclude=["test"]),
+    data_files=[
+        ("share/ament_index/resource_index/packages", [f"resource/{package_name}"]),
+        (f"share/{package_name}", ["package.xml"]),
+        (f"share/{package_name}/config", glob("config/*")),
+        (f"share/{package_name}/launch", glob("launch/*")),
+    ],
+    install_requires=["setuptools"],
+    zip_safe=True,
+    maintainer="BlindTouch Maintainers",
+    maintainer_email="maintainers@example.com",
+    description="Shared ROS helpers and constants for BlindTouch packages.",
+    license="MIT",
+    entry_points={"console_scripts": []},
+)
